2f7226057f41627669b1b97cdf00816a3d9f2dc8
Davide Wylter Di Bernardo <dbr.davide@gmail.com>
2019-01-11 10:46:35 +0100

    -Set timer -Set font

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
 
     [SerializeField]
     private GameObject m_pauseMenu;
+    [SerializeField]
+    private float m_startTime;
+    [SerializeField]
+    private TextMeshProUGUI m_timerText;
 
+    private float m_remeiningTime;
     private Animator m_pauseAnimator;
 
     private void Start() {
         m_pauseAnimator = m_pauseMenu.GetComponent<Animator>();
+        m_remeiningTime = m_startTime + 1f;
+    }
+
+    private void Update() {
+        UpdateTimer();
     }
 
     public void Pause() {
+        Time.timeScale = 0;
         m_pauseMenu.SetActive(true);
         m_pauseAnimator.SetTrigger("Pause");
     }
@@ -24,6 +36,12 @@ public class GameController : MonoBehaviour
     }
 
     public void DismissPause() {
+        Time.timeScale = 1;
         m_pauseMenu.SetActive(false);
+    }
+
+    private void UpdateTimer() {
+        m_remeiningTime -= Time.deltaTime;
+        m_timerText.SetText("TIME: " + (int) m_remeiningTime);
     }
 }
---
+++ Assets/Scripts/LoadHighScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadHighScore : MonoBehaviour
+{
+
+    void Start(){
+        GetComponent<TextMeshProUGUI>().SetText("HIGHSCORE: " + PlayerPrefs.GetInt("highscore"));
+    }
+
+}
